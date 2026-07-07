1020e8902e9f0c89d2e26092a53efab21497a158
nxstray <afwanapriansyah@gmail.com>
2026-07-07 10:15:17 +0700

    feat: enable scheduling and enhance test context for application

--- src/test/java/com/gemono/backend/BackendApplicationTests.java
+++ src/test/java/com/gemono/backend/BackendApplicationTests.java
@@ -1,9 +1,13 @@
 package com.gemono.backend;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@DisplayName("Application Context Sanity Test")
 class BackendApplicationTests {
 
 	@Test
